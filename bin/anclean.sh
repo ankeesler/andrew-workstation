24c3595d54b6e75503baaff1f5e2c1a5a925ec0f
Andrew Keesler <ankeesler1@gmail.com>
2020-04-07 13:42:16 -0400

    bin/anclean.sh: do more.

--- bin/anclean.sh
+++ bin/anclean.sh
@@ -4,5 +4,9 @@ set -e
 
 docker ps -a -q | xargs docker rm
 docker image prune --force
+docker system prune --all --force --volumes
 
 brew cleanup
+
+du -sh /System/Library/Caches/com.apple.coresymbolicationd/data
+sudo rm /System/Library/Caches/com.apple.coresymbolicationd/data
