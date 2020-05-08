65395e4f5b068be4adec30b107f0b360c16edeee
Andrew Keesler <ankeesler@gmail.com>
2020-05-08 09:04:52 -0400

    Fix the yesterday/tomorrow aliases.

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,8 +8,8 @@ pushd "$andrew_workstation"
   brew bundle
 popd
 
-go get golang.org/x/tools/...
-go get github.com/rogpeppe/godef
+go get -u golang.org/x/tools/...
+go get -u github.com/rogpeppe/godef
 
 if ! bundle 1>/dev/null 2>/dev/null; then
   sudo gem install bundler
